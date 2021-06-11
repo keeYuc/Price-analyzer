5cecbfae3d2a547d97e2200d5956294563edf349
keeYuc <@keeyu.cqy>
2021-06-11 12:14:06 +0800

    dataget

--- server/go.mod
+++ server/go.mod
@@ -3,6 +3,8 @@ module server
 go 1.16
 
 require (
+	github.com/klauspost/compress v1.13.0 // indirect
+	github.com/tidwall/pretty v1.1.0 // indirect
 	go.mongodb.org/mongo-driver v1.5.3
 	gopkg.in/yaml.v2 v2.4.0
 )
--- server/main.go
+++ server/main.go
@@ -1,34 +1,17 @@
 package main
 
 import (
-	"encoding/json"
+	"compress/gzip"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"server/config"
 )
 
-func main() {
-	url := "https://xueqiu.com/service/v5/stock/screener/quote/list?page=1&size=90&order=desc&orderby=percent&order_by=percent&market=CN&type=sh_sz&_=1623336144629s"
-	request, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		panic(err)
-	}
-	for _, v := range config.Get().Header {
-		request.Header.Set(v.Key, v.Value)
-	}
-	c := http.Client{}
-	response, err := c.Do(request)
-	if err != nil {
-		fmt.Println(err)
-	}
+// "github.com/tidwall/gjson"
 
-	datas, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		panic(err)
-	}
-	f := make(map[string]interface{})
-	json.Unmarshal(datas, f)
-	fmt.Println(f)
-	defer response.Body.Close()
+func main() {
+
+	// gjson.Get(string(datas))
+
 }
