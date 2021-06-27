e8c25e96abcbef14bb99e675b53f0e674579bd58
keeYuc <52500690@qq.com>
2021-06-27 19:35:55 +0800

    update mongo

--- server/main.go
+++ server/main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/PuerkitoBio/goquery"
+	"server/mongo"
 )
 
 func main() {
-	fd, err := os.Open("./xxx.html")
-	if err != nil {
-		fmt.Println(1, err)
-		return
-	}
-	g, err := goquery.NewDocumentFromReader(fd)
-	if err != nil {
-		fmt.Println(1, err)
-		return
-	}
-	g.Find("#app").Filter("a[href]")
-	if err != nil {
-		fmt.Println(3, err)
-		return
-	}
+	// fd, err := os.Open("./xxx.html")
+	// if err != nil {
+	// 	fmt.Println(1, err)
+	// 	return
+	// }
+	// g, err := goquery.NewDocumentFromReader(fd)
+	// if err != nil {
+	// 	fmt.Println(1, err)
+	// 	return
+	// }
+	// g.Find("#app").Filter("a[href]")
+	// if err != nil {
+	// 	fmt.Println(3, err)
+	// 	return
+	// }
 	// fmt.Println(s)
+	mongo.GetMg()
 }
--- server/mongo/mongo.go
+++ server/mongo/mongo.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"server/config"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -20,6 +21,50 @@ func init() {
 	initMongo()
 }
 
+const (
+	main  = "main"
+	day_1 = "day_1"
+	day_2 = "day_2"
+	day_3 = "day_3"
+	m60_1 = "day_1"
+	m60_2 = "day_2"
+	m60_3 = "day_3"
+	m60_4 = "day_4"
+	m60_5 = "day_5"
+	m60_6 = "day_6"
+)
+
+func createIndex(m *mongo.Database) {
+	c_main := mg.Collection(main)
+	c_main.Indexes().CreateMany(context.TODO(), []mongo.IndexModel{
+		{
+			Keys:    bson.D{{"id", 1}},
+			Options: options.Index().SetUnique(true).SetName("id"),
+		},
+	})
+	c_day1 := mg.Collection(day_1)
+	c_day1.Indexes().CreateMany(context.TODO(), []mongo.IndexModel{
+		{
+			Keys:    bson.D{{"id", 1}},
+			Options: options.Index().SetName("id"),
+		},
+	})
+	c_day2 := mg.Collection(day_2)
+	c_day2.Indexes().CreateMany(context.TODO(), []mongo.IndexModel{
+		{
+			Keys:    bson.D{{"id", 1}},
+			Options: options.Index().SetName("id"),
+		},
+	})
+	c_day3 := mg.Collection(day_3)
+	c_day3.Indexes().CreateMany(context.TODO(), []mongo.IndexModel{
+		{
+			Keys:    bson.D{{"id", 1}},
+			Options: options.Index().SetName("id"),
+		},
+	})
+}
+
 func initMongo() {
 	// uri := "mongodb+srv://<username>:<password>@<cluster-address>/test?w=majority"
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(config.Get().Mongo.Uri))
@@ -31,5 +76,6 @@ func initMongo() {
 		panic(err)
 	}
 	mg = client.Database(config.Get().Mongo.Database)
+	createIndex(mg)
 	fmt.Println("Successfully connected and pinged.")
 }
