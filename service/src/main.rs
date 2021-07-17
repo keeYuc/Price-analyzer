fd0d6939277599c9aef7055f3efe48a76fdfe897
keeYuc <52500690@qq.com>
2021-07-18 00:26:20 +0800

    exchange

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    println!("get a request");
+    HttpResponse::Ok().body("hello")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
